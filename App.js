bf78f02abaaf994436478f036464a6a48ab25844
Harsh Patel <harsh.coderc@gmail.com>
2021-04-21 10:03:19 +0530

    Added App bar with burger menu

--- App.js
+++ App.js
@@ -1,21 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useRoute } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import "react-native-gesture-handler";
+import {
+    Appbar,
+    Button,
+    Menu,
+    Provider as PaperProvider,
+    Title,
+} from "react-native-paper";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <PaperProvider>
+            <StatusBar />
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        header: (props) => <CustomNavigationBar {...props} />,
+                    }}
+                >
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Details" component={DetailsScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+function CustomNavigationBar({ navigation, previous }) {
+    const route = useRoute();
+
+    const [visible, setVisible] = React.useState(false);
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
+
+    return (
+        <Appbar.Header>
+            {previous ? (
+                <Appbar.BackAction onPress={navigation.goBack} />
+            ) : null}
+            <Appbar.Content title={route.name} />
+            <Menu
+                visible={visible}
+                onDismiss={closeMenu}
+                anchor={
+                    <Appbar.Action
+                        icon="menu"
+                        color="white"
+                        onPress={openMenu}
+                    />
+                }
+            >
+                <Menu.Item
+                    onPress={() => {
+                        closeMenu();
+                    }}
+                    title="Option 1"
+                />
+                <Menu.Item
+                    onPress={() => {
+                        closeMenu();
+                    }}
+                    title="Option 2"
+                />
+                <Menu.Item
+                    onPress={() => {
+                        navigation.navigate(
+                            route.name === "Home" ? "Details" : "Home"
+                        );
+                        closeMenu();
+                    }}
+                    title={route.name === "Home" ? "Details" : "Home"}
+                />
+            </Menu>
+        </Appbar.Header>
+    );
+}
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={style.container}>
+            <Title>Home Screen</Title>
+            <Button
+                mode="contained"
+                onPress={() => navigation.navigate("Details")}
+            >
+                Go to details
+            </Button>
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={style.container}>
+            <Title>Details Screen</Title>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
